83b4ed85bf4a18ed5814bf92b52c2b3c0d3507b6
Max Kossek <mkossek@asu.edu>
2019-11-21 12:50:10 -0700

    Add Start up java file and change Text Editor

---
+++ CSE_360_Project/src/StartUp.java
@@ -0,0 +1,77 @@
+import java.awt.BorderLayout;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class StartUp extends JFrame {
+	
+	// Calls the constructor
+	public static void main(String[] args) {
+		new StartUp();
+	}
+	
+	// Generates the start up screen
+	public StartUp() {
+		
+		// Create Window and set the title to "TextFormat"
+		JFrame startWindow = new JFrame("TextFormat");
+		startWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Create panel for layout management
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		
+		// Create a non-editable Text Area to the program
+		JTextArea description = new JTextArea();
+		description.setEditable(false);
+		description.setText("Load an existing file, or create a new file by clicking “Create New File”.\n" + 
+				"\n" + "Supported file formats: .txt\n" + 
+				"\n" + "Read the User Guide for detailed instructions on how to use the program.");
+		description.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		// Button panel
+		JPanel btnPanel = new JPanel();
+		
+		// Create Buttons
+		JButton btnOpen = new JButton("Open existing file");
+		// Open file chooser
+		btnOpen.addActionListener(e -> {
+			String fileReturn = fileChoose();
+			description.setText(fileReturn);
+			TextEditor textEditor = new TextEditor(fileReturn);
+		});
+		btnPanel.add(btnOpen);
+		JButton btnCreate = new JButton("Create new file");
+		btnCreate.addActionListener(e -> {
+			description.setText("");
+			description.setEditable(true);
+			TextEditor textEditor = new TextEditor();
+		});
+		btnPanel.add(btnCreate);
+		
+		// Add elements to the panel
+		panel.add(description, BorderLayout.PAGE_START);
+		panel.add(btnPanel, BorderLayout.PAGE_END);
+		
+		// Add panel to the window
+		startWindow.getContentPane().add(panel);
+		startWindow.pack();
+		startWindow.setVisible(true);
+	}
+	
+	
+	// Method for choosing a .txt file
+	// @return String with file name if successful, else error message
+	public String fileChoose() {
+		JFileChooser fc = new JFileChooser("Open existing file");
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Plaintext files", "txt");
+		fc.setFileFilter(filter);
+		int returnVal = fc.showOpenDialog(getParent());
+		
+		// Return name of the file is successful, else "unable to open file"
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			return fc.getSelectedFile().getName();
+		}
+		else
+			return "Unable to open file";
+	}
+}
--- CSE_360_Project/src/TextEditor.java
+++ CSE_360_Project/src/TextEditor.java
@@ -1,9 +1,72 @@
+import java.awt.BorderLayout;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class TextEditor {
+import javax.swing.*;
 
-	public static void main(String[] args) {
+public class TextEditor extends JFrame {
+	
+	// Constructor for the text editor when no file chosen
+	public TextEditor() {
+		window();
+	}
+	
+	// Overload constructor for the text editor
+	// @param file, chosen by the user
+	public TextEditor(String fileName) {
+		processFile(fileName);
+		window();
+	}
+
+	// Text Editing window
+	private void window() {
 		
+		// Create Window and set the title to "TextFormat"
+		JFrame window = new JFrame("TextFormat");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Create panel for layout management
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
 
+		// Create Text Area, and set the text area to the contents of the input file
+		JTextPane textArea = new JTextPane();
+		textArea.setText("This should be the content of the file, or empty if no file is loaded.");
+		
+		panel.add(textArea, BorderLayout.PAGE_START);
+		
+		JButton btnSaveAs = new JButton("Save As...");
+		panel.add(btnSaveAs);
+		
+		window.getContentPane().add(panel);
+		window.pack();
+		window.setVisible(true);
+	}
+	
+	
+	// Method for reading in the text file
+	// @param fileName, the name of the plaintext file
+	private void processFile(String fileName) {
+		
+		// ToDo: Add error checking etc.
+		try {
+			BufferedReader fr = new BufferedReader(new FileReader(fileName));
+			String line = fr.readLine();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Unable to read file.");
+			e.printStackTrace();
+		}
+	}
+	
+	// Method for processing each line from the input file
+	// @param line, a line that was read from the file
+	private void processLine(String line) {
+		
 	}
 
 }
