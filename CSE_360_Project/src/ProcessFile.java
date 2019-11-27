063f62b3449508333e13651a6a3abe342d6488db
YashMilk <ybabaria@asu.edu>
2019-11-27 14:42:02 -0700

    Added some comments on ProcessFile.java
    I added what I felt should be implemented to the class which subject to change.

--- CSE_360_Project/src/ProcessFile.java
+++ CSE_360_Project/src/ProcessFile.java
@@ -3,12 +3,18 @@ import java.io.*;
 
 public class ProcessFile {
 	private String fileString = "";
+	// Create string to format the file string
 	public ProcessFile(File file){
 		try {
 			BufferedReader fr = new BufferedReader(new FileReader(file));
 			String line;
 			while ((line = fr.readLine()) != null) {
 				this.fileString += line + "\n";
+				/**
+				 * if(line.chArt(0) == "-"){
+				 * 		whichFlag(line.chArt(1),the next line);
+				 * } or something like this
+				 */
 			}
 			fr.close();
 		} catch (FileNotFoundException e) {
@@ -19,13 +25,40 @@ public class ProcessFile {
 			e.printStackTrace();
 		}
 	}
+	/* Find a way to save the file into a txt when user clicks on save as
+	public File saveAs(){
+		File savingFile = new File();
+	}*/
 	
 	public String printFile(){
 		return fileString;
 	}
 	
-	/* Find a way to save the file into a txt when user clicks on save as
-	public File saveAs(){
-		File savingFile = new File();
-	}*/
+	// Find which flag to implement to the next line or strings
+	public void whichFlag(char letter, String line){
+		
+	}
+	
+	// -t
+	public void title(){
+		
+	}
+	
+	//-e
+	public void emptySpace(){
+		
+	}
+	//-r
+	//-l
+	
+	//-s
+	//-d
+	
+	//-i
+	//-b
+	//-n
+	
+	//-1
+	//-2
+	
 }
